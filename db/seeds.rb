1e03fa7f545cb0e5f7183df88fda6b8d5314f979
charlieperson <charlie.person@hyperisland.se>
2016-04-03 23:46:34 +0100

    Adds users and their profiles into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def create_user(username, password)
+  User.create(username: username, password_digest: password)
+end
+
+def create_profile(user, gender, birth_year, first_name, last_name)
+  user.profile = Profile.create(gender: gender, birth_year: birth_year, first_name: first_name, last_name: last_name)
+end
+
+
+
+fiorina = create_user('Fiorina', '12345')
+trump = create_user('Trump', '12345')
+carson = create_user('Carson', '12345')
+clinton = create_user('Clinton', '12345')
+
+create_profile(fiorina, 'female', 1954, 'Carly', 'Fiorina')
+create_profile(trump, 'male', 1946, 'Donald', 'Trump')
+create_profile(carson, 'male', 1951, 'Ben', 'Carson')
+create_profile(clinton, 'female', 1947, 'Hillary', 'Clinton')
