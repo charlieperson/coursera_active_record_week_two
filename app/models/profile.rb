326c5a1ab44cb79f157b591b0d258eb1103f5133
charlieperson <charlie.person@hyperisland.se>
2016-04-05 11:37:30 +0100

    Adds custom validator that ensures that a user's first name and last name are not both nil

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,12 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :last_and_first
+
+
+  def last_and_first
+    if last_name.nil? && first_name.nil?
+      errors.add(:last_name, 'last name and first name cannot be null!')
+    end
+  end
 end
