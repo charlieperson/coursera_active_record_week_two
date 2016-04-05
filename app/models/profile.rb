93c321597a06ec5ef2e62b97c43b1b45d8edcf7b
charlieperson <charlie.person@hyperisland.se>
2016-04-05 11:40:47 +0100

    Gender cannot be anything aside from male or female

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,9 @@ class Profile < ActiveRecord::Base
 
   validate :last_and_first
 
+  validates :gender, inclusion: { in: %w(male female),
+    message: "%{value} is not a valid gender" }
+
 
   def last_and_first
     if last_name.nil? && first_name.nil?
